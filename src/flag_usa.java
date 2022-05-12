5d25e8857c644a1187ff2ba2b518f0d99281f58a
ngakezzy <ngakezzy@gmail.com>
2022-05-12 09:22:27 +0700

    done flag  usa

--- src/flag_usa.java
+++ src/flag_usa.java
@@ -1,16 +1,27 @@
 public class flag_usa {
     public static void main(String[] args) {
+        System.out.print("<-------------------- USA ------------------->\n  \n");
 
-        for (int i = 0; i <= 20; i++) {
-            for (int x = 0; x <= 5; x++) {
-                if (i <= 5)
-                    System.out.print("*  ");
-                else
-                    System.out.print("= ");
+        for (int i = 0; i <= 3; i++) {
+                for (int j= 0; j <= 20; j++){
+                    if (j <= 5)
+                        System.out.print("*  ");
+                    else
+                        System.out.print("= ");
+                }
+            System.out.print("\n");
+            }
+
+        for(int x = 0; x <= 4; x++) {
+            for(int z = 0; z <= 23; z++){
+                System.out.print("= ");
             }
             System.out.print("\n");
         }
 
-
     }
+
 }
+
+
+
